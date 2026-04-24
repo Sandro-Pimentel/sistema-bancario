1ff92e8fa118897087ec316f8a11fb8a255a1dd3
Sandrowski <sandro.pimentel.junior@gmail.com>
2026-04-24 15:00:40 -0300

    fix: Correção de bugs na criação de conta, no saque e no depósito

--- conta.py
+++ conta.py
@@ -8,47 +8,47 @@ class Conta:
         self._cliente = cliente
         self._historico = Historico()
 
-        @classmethod
-        def nova_conta(cls, cliente, numero):
-            return cls(numero, cliente)
+    @classmethod
+    def nova_conta(cls, cliente, numero):
+        return cls(numero, cliente)
 
-        @classmethod
-        def saldo(self):
-            return self._saldo
-        
-        @classmethod
-        def numero(self):
-            return self._numero
-        
-        @classmethod
-        def agencia(self):
-            return self._agencia
-        
-        @classmethod
-        def cliente(self):
-            return self._cliente
-        
-        @classmethod
-        def historico(self):
-            return self._historico
-        
-        def sacar(self, valor):
-            saldo = self.saldo
-            excedeu_saldo = valor > saldo
+    @property
+    def saldo(self):
+        return self._saldo
+    
+    @property
+    def numero(self):
+        return self._numero
+    
+    @property
+    def agencia(self):
+        return self._agencia
+    
+    @property
+    def cliente(self):
+        return self._cliente
+    
+    @property
+    def historico(self):
+        return self._historico
+    
+    def sacar(self, valor):
+        saldo = self.saldo
+        excedeu_saldo = valor > saldo
 
-            if excedeu_saldo:
-                print("\n--- Erro ao processar operação! Saldo insuficiente. ---")
+        if excedeu_saldo:
+            print("\n--- Erro ao processar operação! Saldo insuficiente. ---")
 
-            elif valor > 0:
-                self._saldo -= valor
-                print("\n--- Saque realizado com sucesso! ---")
-                return True
-            else: 
-                print("\n --- Erro ao processar operação! Valor informado inválido. ---")
+        elif valor > 0:
+            self._saldo -= valor
+            print("\n--- Saque realizado com sucesso! ---")
+            return True
+        else: 
+            print("\n --- Erro ao processar operação! Valor informado inválido. ---")
 
-            return False
-        
-        def depositar(self, valor):
+        return False
+    
+    def depositar(self, valor):
             if valor > 0:
                self._saldo += valor
                print("\n--- Depósito realizado com sucesso! ---")
--- conta_corrente.py
+++ conta_corrente.py
@@ -1,4 +1,5 @@
 from conta import Conta
+from saque import Saque
 
 class ContaCorrente(Conta):
     def __init__(self, numero, cliente, limite=500, limite_saques=3):
@@ -6,27 +7,26 @@ class ContaCorrente(Conta):
         self.limite = limite
         self.limite_saques = limite_saques
 
-        def sacar(self, valor):
-            numero_saques = len([transacao for transacao in self.historico.transacoes if transacao["tipo"] == Saque.__name__])
+    def sacar(self, valor):
+        numero_saques = len([transacao for transacao in self.historico.transacoes if transacao["tipo"] == Saque.__name__])
 
-            excedeu_limite = valor > self.limite
-            excedeu_saques = numero_saques >= self.limite_saques
+        excedeu_limite = valor > self.limite
+        excedeu_saques = numero_saques >= self.limite_saques
 
-            if excedeu_limite:
-                print("\n--- Erro ao processar operação! Valor de saque excede o limite. ---")
+        if excedeu_limite:
+            print("\n--- Erro ao processar operação! Valor de saque excede o limite. ---")
 
-            elif excedeu_saques:
-                print("\n--- Erro ao processar operação! Número máximo de saques excedido. ---")
-            
-            else:
-                return super().sacar(valor)
-            
-            return False
+        elif excedeu_saques:
+            print("\n--- Erro ao processar operação! Número máximo de saques excedido. ---")
+        
+        else:
+            return super().sacar(valor)
+        
+        return False
 
-
-        def __str__(self):
-            return f"""
-            Agência:\t{self.agencia}
-            C/C:\t{self.numero}
-            Titular:\t{self.cliente.nome}
-            """
+    def __str__(self):
+        return f"""
+        Agência:\t{self.agencia}
+        C/C:\t{self.numero}
+        Titular:\t{self.cliente.nome}
+        """
